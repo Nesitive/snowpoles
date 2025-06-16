cc5c149f8c9046e16c3ba81ce89630b5329f4600
Nesitive <wbeau12@yahoo.com>
2025-06-16 10:54:05 -0500

    Detect full image based on filenames (which should be unique)

--- src/dataset.py
+++ src/dataset.py
@@ -35,6 +35,7 @@ import os
 from pathlib import Path
 
 
+
 # Define a function to sample every third photo
 ## Only used for experiments
 def sample_every_x(group, x):
@@ -56,6 +57,10 @@ def train_test_split(csv_path, image_path):
 
     ## check to make sure we only use images that exist
     all_images = list(Path(image_path).rglob("*.JPG"))
+    global parents
+    parents = {}
+    for i in all_images:
+        parents[str(i).split("/")[-1]] = str(i)
     filenames = [img.name for img in all_images]
     valid_samples = valid_samples[
         valid_samples["filename"].isin(filenames)
@@ -130,12 +135,10 @@ class snowPoleDataset(Dataset):
     def __getitem__(self, index):
         cameraID = self.data.iloc[index]["filename"].split("_")[
             0
-        ]  ## may need to update this.
+        ]  ## may need to update this. *Yeah, you think?* -Nesitive
         filename = self.data.iloc[index]["filename"]
 
-        image = cv2.imread(
-            f"{self.path}/{cameraID}/{self.data.iloc[index]['filename']}"
-        )
+        image = cv2.imread(parents[self.data.iloc[index]["filename"]])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         orig_h, orig_w, channel = image.shape
 
